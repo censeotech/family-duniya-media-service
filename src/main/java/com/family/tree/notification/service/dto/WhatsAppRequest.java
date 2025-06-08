adee432660f45284238e9a94404c606be9a0112e
anwar.ong <anwar.ong@gmail.com>
2025-06-08 19:56:33 +0530

    initial

--- src/main/java/com/family/tree/notification/service/dto/WhatsAppRequest.java
+++
@@ -1,9 +0,0 @@
-package com.family.tree.notification.service.dto;
-
-import lombok.Data;
-
-@Data
-public class WhatsAppRequest {
-    private String to;     // E.g. +1234567890
-    private String message;
-}
